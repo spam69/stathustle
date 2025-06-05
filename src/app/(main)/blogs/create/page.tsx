ef4175b8306ef06987a988e3f6ea94daa4893681
Umer Farooq <umer.farooq@wamolabs.com>
2025-06-05 20:42:40 +0000

    clicking the button raised this error

--- src/app/(main)/blogs/create/page.tsx
+++ src/app/(main)/blogs/create/page.tsx
@@ -9,7 +9,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from "@/components/ui/form";
 import { useSearchParams, useRouter } from 'next/navigation';
 import Link from "next/link";
 import { useAuth } from '@/contexts/auth-context';
